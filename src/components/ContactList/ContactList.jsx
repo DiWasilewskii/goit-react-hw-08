006142db54fdd335ba5cb94420e5bddef7c31358
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-09-26 19:45:51 +0300

    up

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,14 @@
 import Contact from "../Contact/Contact.jsx";
 import css from "./ContactList.module.css";
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice.js";
-import { selectFilters } from "../../redux/filtersSlice.js";
+import { selectVisibleContacts } from "../../redux/selectors.js";
 
 export default function ContactList() {
-  const phoneBook = useSelector(selectContacts);
-  const filteredBook = useSelector(selectFilters);
-
-  const visibleContacts = phoneBook.filter(phone =>
-    phone.name.toLowerCase().includes(filteredBook.toLowerCase())
-  );
+  const filteredBook = useSelector(selectVisibleContacts);
 
   return (
     <ul className={css.contactList}>
-      {visibleContacts.map(data => (
+      {filteredBook.map(data => (
         <li key={data.id} className={css.contact}>
           <Contact data={data} />
         </li>
